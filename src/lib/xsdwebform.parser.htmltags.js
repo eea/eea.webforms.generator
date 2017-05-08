d677a5de79c089db95925217c33825cddd836575
George Bouris <gb@eworx.gr>
2017-05-08 15:45:38 +0300

    Adding CSS rules

--- src/lib/xsdwebform.parser.htmltags.js
+++ src/lib/xsdwebform.parser.htmltags.js
@@ -592,7 +592,7 @@ app.config(["$translateProvider",function($translateProvider){
 /**
 * WebFormAppCtrl: Main controller
 */
-function WebFormAppCtrl($scope, $http, $timeout) {
+function WebFormAppCtrl($scope, $http, $timeout, $window) {
 
 	$scope.field = {};  
 	$scope.multipleIndex = 1;
@@ -608,6 +608,35 @@ function WebFormAppCtrl($scope, $http, $timeout) {
 		return false;
 	};
 
+	$scope.printPreview = function(){
+		var conversionLink = [formApplicationUrl("/download/convert", urlProperties.baseURI, urlProperties.sessionID, urlProperties.fileID), "&conversionId=", HTMLconversionNumber].join("");
+	$window.open(conversionLink, '_blank');
+	}
+
+	$scope.toggleValidation = function(){
+		$scope.ValidationDisabled = !$scope.ValidationDisabled;
+	}
+
+	$scope.save = function(){
+		dataRepository.saveInstance($scope.Webform);
+	}
+
+	$scope.close = function(){
+
+		if (urlProperties.baseURI == ''){
+				urlProperties.baseURI = "/";
+		};
+   	 	var windowLocation = (urlProperties.envelope && urlProperties.envelope.length > 0) ? urlProperties.envelope : urlProperties.baseURI;
+    	if ($scope.Webform.$dirty){
+        	if ($window.confirm('You have made changes in the questionnaire! \\n\\n Do you want to leave without saving the data?')){
+            	window.location = windowLocation;
+        	}
+    	}
+    	else {
+       	 	window.location = windowLocation;
+    	}
+
+	}
 
 	$scope.addRow = function() {
 		alert('Row');
