5dd4fbb10995212e15bbe52e989c318e69622a6a
George Bouris <gb@eworx.gr>
2017-05-19 14:01:04 +0300

    Delete Form row bug fixed

--- src/webform.js
+++ src/webform.js
@@ -22,15 +22,15 @@ function WebFormAppCtrl($scope, $http, $timeout, $window,  $translate, $compile)
 	
 	$scope.addRow = function(frm, group) {
 		$scope.multipleIndex = $scope.groups[frm][group].length;
-		$scope.groups[frm][group].push(++$scope.multipleIndex);
-		var tmi = $scope.multipleIndex;
+		var tmi = $scope.multipleIndex++;
+		$scope.groups[frm][group].push(frm + '-' + tmi + '-' + group);
 		$timeout ( () => {
-			$(`#group-area-${$scope.multipleIndex}-${group}`).prepend( $compile(`<div class="row"><button type="button"  title="{{'deleterow' | translate}}"class="deleterowbutton" ng-click="deleteRow('${frm}', '${group}', '${tmi}')"></button></div>`)($scope));
+			$(`#group-area-${$scope.multipleIndex}-${group}`).prepend( $compile(`<div class="row"><button type="button"  title="{{'deleterow' | translate}}"class="deleterowbutton" ng-click="deleteRow('${frm}', '${group}', ${tmi})"></button></div>`)($scope));
 		}, 10);
 	};
 
 	$scope.deleteRow = function(frm, group, index) {
-		$scope.groups[frm][group].splice($scope.groups[frm][group].indexOf(index), 1);
+		$scope.groups[frm][group].splice($scope.groups[frm][group].indexOf(frm + '-' + index + '-' + group), 1);
 	};
 
 }
