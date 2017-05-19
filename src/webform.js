6e2633f93c0c7b712998f06e65743a3bdd9467f8
George Bouris <gb@eworx.gr>
2017-05-19 12:04:16 +0300

    Form CSS changes

--- src/webform.js
+++ src/webform.js
@@ -22,6 +22,7 @@ function WebFormAppCtrl($scope, $http, $timeout, $window,  $translate, $compile)
 	
 	$scope.addRow = function(frm, group) {
 		$scope.multipleIndex = $scope.groups[frm][group].length;
+		console.log("$scope.multipleIndex", $scope.multipleIndex);
 		$scope.groups[frm][group].push(++$scope.multipleIndex);
 		$timeout ( () => {
 			$(`#group-area-${$scope.multipleIndex}-${group}`).prepend( $compile(`<div class="row"><button type="button"  title="{{'deleterow' | translate}}"class="deleterowbutton" ng-click="deleteRow('${frm}', '${group}', '${$scope.multipleIndex}')"></button></div>`)($scope));
