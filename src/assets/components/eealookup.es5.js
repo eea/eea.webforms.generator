86a261336f04b1efd083ba9ad0bd1fe5abc80575
George Bouris <gb@eworx.gr>
2017-05-29 18:00:18 +0300

    Adding Lookup field

---
+++ src/assets/components/eealookup.es5.js
@@ -0,0 +1,41 @@
+'use strict';
+
+app.config(['$locationProvider', function ($locationProvider) {
+	$locationProvider.html5Mode({
+		enabled: true,
+		requireBase: false
+	});
+}]);
+
+app.component("lookup", {
+	template: '<select ng-model="$ctrl.ngModel" ng-change="$ctrl.updateLookup()" name="$ctrl.name" ng-options="option{{$ctrl.luValue}} as option{{$ctrl.luOption}} for option in $ctrl.data | orderBy:\'{{$ctrl.luOrder}}\'" class="slookup"  style="box-shadow: 0px!imporant;" required><option value="">-----</option></select>',
+	bindings: {
+		ngModel: '@',
+		lookup: '@',
+		name: '@',
+		luValue: '@',
+		luOption: '@',
+		luOrder: '@',
+		luData: '@',
+		autoselect: '@',
+		hideonautoselect: '@',
+		scp: '='
+	},
+	controller: function controller($http, $translate, $location) {
+		var parent = this;
+		this.$onInit = function () {
+			(function () {
+				$http.get(parent.lookup).then(function (response) {
+					parent.data = response.data[parent.luData];
+					if (parent.autoselect) {
+						parent.ngModel = $location.search()['countrycode'];
+					}
+					if (parent.hideonautoselect == 1 && $location.search()['countrycode']) {
+						parent.scp['h_' + parent.name] = 1;
+					}
+				});
+			})();
+			this.updateLookup = function () {};
+		};
+	}
+});
