2e06ab7346d9b090e9ead732fbe9021d55ba28a9
George Bouris <gb@eworx.gr>
2017-05-29 13:41:44 +0300

    Adding Lookup field

--- src/assets/components/eealookup.js
+++ src/assets/components/eealookup.js
@@ -1,11 +1,12 @@
 app.component("lookup",{
-	template: '<select ng-model="$ctrl.ngModel" ng-change="$ctrl.updateLookup()" name="$ctrl.name" ng-options="option{{$ctrl.luValue}} as option{{$ctrl.luOption}} for option in $ctrl.data" class="slookup"  style="box-shadow: 0px!imporant;" required></select>',
+	template: '<select ng-model="$ctrl.ngModel" ng-change="$ctrl.updateLookup()" name="$ctrl.name" ng-options="option{{$ctrl.luValue}} as option{{$ctrl.luOption}} for option in $ctrl.data | orderBy:\'{{$ctrl.luOrder}}\'" class="slookup"  style="box-shadow: 0px!imporant;" required></select>',
 	bindings: {
 		ngModel: '@',
 		lookup: '@',
 		name: '@',
 		luValue : '@' ,
 		luOption : '@' ,
+		luOrder : '@' ,
 		luData: '@',
 		scp: '='
 	},
