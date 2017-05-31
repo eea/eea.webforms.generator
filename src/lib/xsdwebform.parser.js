a4b4168f0d423e0de08500a1e9eeb1a70500b8c6
George Bouris <gb@eworx.gr>
2017-05-31 15:00:43 +0300

    Renamed .tags.js library to .xsd.

--- src/lib/xsdwebform.parser.js
+++ src/lib/xsdwebform.parser.js
@@ -7,7 +7,7 @@
 'use strict';
 
 import { XmlDocument } from 'xmldoc';
-import XsdTagParser from './xsdwebform.parser.tags.js';
+import XsdTagParser from './xsdwebform.parser.xsd.js';
 import HtmlTagParser from './xsdwebform.parser.html.js';
 import XsltGenerator from './xsdwebform.parser.xslt.js';
 import XSDWebFormParserLog from './xsdwebform.parser.log.js';
--- src/lib/xsdwebform.parser.xsd.js
+++ src/lib/xsdwebform.parser.xsd.js
@@ -1,5 +1,5 @@
 /**
- * @file xsdwebform.parser.tags.js
+ * @file xsdwebform.parser.xsd.js
  * XSD Schema to HTML5 Web Form
  * @author George Bouris <gb@eworx.gr>
  * @copyright Copyright (C) 2017 EEA, Eworx, George Bouris. All rights reserved.
