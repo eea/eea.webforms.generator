5939b19608b2ad6ad05f57be00e6576eb71fc4e9
George Bouris <gb@eworx.gr>
2017-06-01 16:16:40 +0300

    Adding Form Testing

--- src/assets/components/eeaformtesting.js
+++ src/assets/components/eeaformtesting.js
@@ -5,10 +5,16 @@ app.component("eeaFormTesting",{
 <script src="./assets/js/test/mocha.min.js"></script>
 <script src="./assets/js/test/chai.min.js"></script>
 <link rel="stylesheet" type="text/css" href="./assets/js/test/mocha.min.css"/>
-
-<div style="width: 30%; min-width: 300px; z-index: 999; position: fixed;bottom: 0; padding: 20px; margin: 4px; border: solid 1px #ccc;background-color:#fff; box-shadow: 5px 5px 5px #888888;">	
+<style>
+#mocha-stats {
+    position: absolute;
+    top: 2px;;
+    right: 10px;
+}
+</style>
+<div style="width: 30%; min-width: 300px; z-index: 999; rgba(255, 255, 255, 0.95); position: fixed;bottom: 0; padding: 10px; margin: 4px; border: solid 1px #ccc;background-color:#fff; box-shadow: 5px 5px 5px #888888;">	
 <b>Testing Module</b><BR>
-<small>Remove &lt;eea-form-testing&gt; component to disable.</small>
+<div style="font-size:11px;">Remove &lt;eea-form-testing&gt;<BR>component to disable.</div>
  <div id="mocha"></div>
 </div>
 `,
