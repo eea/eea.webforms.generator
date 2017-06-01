7597d86a5602160740907af56f5c18884fded3ae
George Bouris <gbouris@gmail.com>
2017-06-01 19:33:00 +0300

    Adding Testing module

--- src/assets/components/eeaformtesting.js
+++ src/assets/components/eeaformtesting.js
@@ -6,6 +6,9 @@ app.component("eeaFormTesting",{
 <script src="./assets/js/test/chai.min.js"></script>
 <link rel="stylesheet" type="text/css" href="./assets/js/test/mocha.min.css"/>
 <style>
+#mocha {
+	margin: 20px 50px;
+}
 #mocha-stats {
 	position: absolute;
 	top: 4px;
@@ -17,7 +20,7 @@ app.component("eeaFormTesting",{
 <div id="testd" style="height: 50%;overflow-y: auto;width: 30%;min-width: 350px; z-index: 999;position: fixed;bottom: 0; padding: 10px; margin: 4px; border: solid 1px #ccc;background-color: rgba(255, 255, 255, 0.99); box-shadow: 5px 5px 5px #888888;">	
  <div onclick="$('#testd').hide();" style="border: 1px solid #333;user-select: none;background-color: #333;width: 30px; height: 30px;line-height: 24px;border-radius: 50%;color: #fff;position: fixed;font-size: 16px; font-weight: 900; text-align: center; left: 2px; bottom: 20px;cursor: pointer;">x</div>
 <b>Testing Module</b><BR>
-<div style="font-size:11px;padding:10px;border-radius:5px;">Remove <b>&lt;eea-form-testing&gt;</b> component<BR>from page source to disable.</div>
+<div style="font-size:9px;padding: 5px 0;border-radius:5px;width: 150px;">Remove <b>&lt;eea-form-testing&gt;</b> component rom page source to disable.</div>
  <div id="mocha"></div>
 </div>
 `,
