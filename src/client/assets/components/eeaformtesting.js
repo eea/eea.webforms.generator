c916a297349914b3f8fff1d0d8caf64bf09ed73d
George Bouris <gb@eworx.gr>
2017-06-14 16:28:00 +0300

    Removed unused css class in EEA Form Testing Component

--- src/client/assets/components/eeaformtesting.js
+++ src/client/assets/components/eeaformtesting.js
@@ -9,10 +9,6 @@ app.component("eeaFormTesting", {
 	font-family: inherit;
 	z-index: 999;
 }
-#mocha-stats canvas {
-    width: 40px;
-    height: 35px;
-}
 #mocha h1 a { 
 	font-size: 13px; 
 	font-weight: 900;
