77cf4ed503d8441ad15aefd888e4dabacecea5ef
George Bouris <gb@eworx.gr>
2017-06-22 15:14:02 +0300

    Updated XSDWebFormParserTags

--- src/lib/xsdwebform.parser.xsd.js
+++ src/lib/xsdwebform.parser.xsd.js
@@ -14,6 +14,7 @@ import XSDWebFormParserError from './xsdwebform.parser.error.js';
  *
  * import { XmlDocument } from 'xmldoc';
  * import XSDWebFormParserLog from './xsdwebform.parser.log.js';
+ * import XSDWebFormParserTags from './xsdwebform.parser.xsd.js';
  *
  * var myXml = new XmlDocument(XML TEXT);
  * var myXmlParser = new XSDWebFormParserTags();
