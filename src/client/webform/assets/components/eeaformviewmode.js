030b57dd9aebe07a90ecae4ad75fb35ac87c17a8
George Bouris <gb@eworx.gr>
2017-06-23 15:10:31 +0300

    Eslinting src/Client

--- src/client/webform/assets/components/eeaformviewmode.js
+++ src/client/webform/assets/components/eeaformviewmode.js
@@ -5,42 +5,41 @@ app.component("eeaFormViewmode", {
 		scp: '='
 	},
 	controller: [function() {
-		var parent = this;
 		this.$onInit = function() {
-				$(function() {
-					var ctimeout = ($('eea-form-testing').length) ? 3000 : 10;
- 
-					setTimeout( function() {
-						$("form.eeaform").each(function(){
-							$(this).find('.formitem :input').each(function(index, item){
-								var valToEnter = $(item).val();
-								if (!valToEnter ||  (item.type == "checkbox" && !$(item).prop("checked")))
-									return;
-								
-								$(item).attr("readonly" , true);
-								$(item).hide();
+			$(function() {
+				var ctimeout = ($('eea-form-testing').length) ? 3000 : 10;
 
-								var tb = $("<button class=\"formitemedit\"><i class=\"fa fa-pencil-square-o\"></i></button>");
-								tb.inp =  $(this);
-								tb.click(function(event) {
-									event.preventDefault();
-									$(item).show();
-									$(item).attr("readonly" , false);
-									$(this).hide();
-									$(item).next(".formitemval").hide();
-								});
+				setTimeout( function() {
+					$("form.eeaform").each(function(){
+						$(this).find('.formitem :input').each(function(index, item){
+							var valToEnter = $(item).val();
+							if (!valToEnter ||  (item.type == "checkbox" && !$(item).prop("checked")))
+								return;
+							
+							$(item).attr("readonly" , true);
+							$(item).hide();
 
-								$(this).before(tb);
-								
-								 if (item.type === "select-one") {
-									valToEnter = $(this).find(":selected").text();
-								} 
-																
-								$(this).after("<span class=\"formitemval\">" + valToEnter + "</span>");
+							var tb = $("<button class=\"formitemedit\"><i class=\"fa fa-pencil-square-o\"></i></button>");
+							tb.inp =  $(this);
+							tb.click(function(event) {
+								event.preventDefault();
+								$(item).show();
+								$(item).attr("readonly" , false);
+								$(this).hide();
+								$(item).next(".formitemval").hide();
 							});
+
+							$(this).before(tb);
+							
+							if (item.type === "select-one") {
+								valToEnter = $(this).find(":selected").text();
+							} 
+															
+							$(this).after("<span class=\"formitemval\">" + valToEnter + "</span>");
 						});
-					}, ctimeout);
-				});
+					});
+				}, ctimeout);
+			});
 		};
 	}]
 });
