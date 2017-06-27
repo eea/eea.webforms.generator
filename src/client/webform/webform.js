febeba5c4dbaac464ac1b00d8098d565eff6770c
George Bouris <gb@eworx.gr>
2017-06-27 13:52:13 +0300

    Updated TODO in webform.js

--- src/client/webform/webform.js
+++ src/client/webform/webform.js
@@ -54,7 +54,7 @@ eea.css.get("webform.all");
 
 /*
 TODO
-- Error Reporting/Form Deactivating Component
+- Error Reporting/Form Deactivating Component or Webform Core or Core
 - Lookup Component. AutoGrouping in case more than once instances have the same attributes 
 - Lookup Component. When adding row, get data from existing instance
 
