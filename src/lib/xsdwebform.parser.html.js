fbf9c7970ef1bb1e3ad57616cf778d5d65178d9b
George Bouris <gb@eworx.gr>
2017-06-27 14:17:09 +0300

    Code/Comments Cleanup

--- src/lib/xsdwebform.parser.html.js
+++ src/lib/xsdwebform.parser.html.js
@@ -44,7 +44,7 @@ class XSDWebFormParserHTMLTags {
 
 		this.XSD_HTML_TYPES = {
 			"xs:integer": "number", //TODO: pattern ? totalDigits
-			"xs:decimal": "number", //TODO: decimal, pattern="[0-9]+([\.,][0-9]+)?" step="0.01"(?) https://stackoverflow.com/questions/15303940/how-to-handle-floats-and-decimal-separators-with-html5-input-type-number
+			"xs:decimal": "number", //TODO: decimal, pattern="[0-9]+([\.,][0-9]+)?" step="0.01" (?) https://stackoverflow.com/questions/15303940/how-to-handle-floats-and-decimal-separators-with-html5-input-type-number
 			"NonNegativeIntegerType": "positivenumber",
 			"xs:date": "date",
 			"DateType": "date",
