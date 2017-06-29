2404da9f01b3977b3c171e4e608c54a6e53c1ec3
George Bouris <gb@eworx.gr>
2017-06-29 12:24:31 +0300

    Adding Data caching at EEA Lookup Component

--- src/client/webform/assets/components/eealookup.js
+++ src/client/webform/assets/components/eealookup.js
@@ -10,7 +10,7 @@ app.config([ '$locationProvider', function($locationProvider) {
 /*
 app.provider('data', function() {
 // Lookup Component. AutoGrouping in case more than once instances have the same attributes & Lookup Component. When adding row, get data from existing instance
-//  Store returned REST/Json data to a Hash Array lookup/url+lu-data etc and check the Hash for existing data e.g. (app.config?) Data[crc32(url string) + "-" + crc32(lu-data string)] (unique?) = json data
+//  Store returned REST/Json data to a Hash Array lookup/url+lu-data etc and check the Hash for existing data e.g. (CRC?) Data[crc32(url string) + "-" + crc32(lu-data string)] (unique?) = json data
 //var DATA_STORAGE = {};
 	this.pushData = function(srv) {
 		srv.url, srv.dataset, srv.data
