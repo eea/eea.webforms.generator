594a6c90e1980de085ac6143c3a763c20035f417
George Bouris <gb@eworx.gr>
2017-06-30 15:06:29 +0300

    Updated EEA xsdwebforms.js TODO

--- src/xsdwebform.js
+++ src/xsdwebform.js
@@ -55,7 +55,7 @@ import XSDWebFormParserTest from './lib/xsdwebform.parser.test.js';
 /**
  * Class XSDWebForm
  * XSD Schema to HTML5
- * PolyForm / PolyGroup (can add rows) / PolymultiSubGroup(todo:tabbed) (Subgroup inside Subgroup), TODO: Array of input type instead of ; separators and maybe PolyPage
+ * PolyForm / PolyGroup (can add rows) / PolymultiSubGroup(todo:tabbed) (Subgroup inside Subgroup), TODO: Array of input type instead of ; separators (can add input field rows) and maybe PolyPage
  */
 export default class XSDWebForm {
 
