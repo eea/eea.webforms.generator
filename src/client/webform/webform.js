5ad4622979b55b7e884ea759fb0f12e55b3bb655
George Bouris <gb@eworx.gr>
2017-06-30 14:50:02 +0300

    Updated EEA Webform.js TODO

--- src/client/webform/webform.js
+++ src/client/webform/webform.js
@@ -9,7 +9,7 @@ app.controller('WebFormAppCtrl', ['$eea', '$scope', '$http', '$timeout', '$windo
 
 /**
 * WebFormAppCtrl: Main controller
-* MultiForm / MiltuiGroup / PolymultiSubGroup (Subgroup inside Subgroup), TODO: Array of input type instead of ; separators and maybe MultiPage
+* PolyForm / PolyGroup / PolymultiSubGroup (Subgroup inside Subgroup), TODO: Array of input type instead of ; separators and maybe MultiPage
 */
 function WebFormAppCtrl($eea, $scope) {
 
